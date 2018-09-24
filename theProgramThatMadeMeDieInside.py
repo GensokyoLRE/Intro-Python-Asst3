42070602107ddb27218b8bdb402864198e7755fe
GensokyoLRE <31522695+GensokyoLRE@users.noreply.github.com>
2018-09-23 22:46:56 -0700

    THE PAIN
    OW

--- theProgramThatMadeMeDieInside.py
+++ theProgramThatMadeMeDieInside.py
@@ -1,12 +1,12 @@
 """
 Program: theProgramThatMadeMeDieInside.py
 Author: Phillip "Hifumi" Cuesta
-Last Mod: 9/22/18
+Last Mod: 9/23/18
 Purpose: Assignment for week 5. But... whyyyyyyyyyyyy doe.
 """
 import random
 
-theList, theRand = [2], random.randint(5, 8)
-while len(theList) != theRand:
-    theList.append(random.randint(theList[-1], (theList[-1] ** 2)))
-print(f'List: {theList}  - Sum: {sum(theList)} - Median: {theList[(len(theList)) // 2]} - Avg: {sum(theList) /2}')
+tL, theRand = [2], random.randint(5, 8)
+while len(tL) != theRand:
+    tL.append(random.randint(tL[-1], (tL[-1] * 2)))
+print('List: %s - Sum: %i - Median: %.0f - Avg: %.2f' % (tL, sum(tL), tL[len(tL)//2] if len(tL) % 2 == 1 else (tL[(len(tL) // 2) - 1] + tL[len(tL) // 2]), sum(tL) / 2))
