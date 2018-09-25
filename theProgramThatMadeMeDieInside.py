af2616a6d872855d349b30f03b83c0146961bdcb
GensokyoLRE <31522695+GensokyoLRE@users.noreply.github.com>
2018-09-25 10:49:30 -0700

    Final Submit

--- theProgramThatMadeMeDieInside.py
+++ theProgramThatMadeMeDieInside.py
@@ -10,4 +10,4 @@ import random
 tL, theRand = [2], random.randint(5, 8)
 while len(tL) != theRand:
     tL.append(random.randint(tL[-1], (tL[-1] * 2)-1))
-print('List: %s - Sum: %i - Median: %.0f - Avg: %.2f' % (', '.join(map(str, tL)), sum(tL), tL[len(tL)//2] if len(tL) % 2 == 1 else (tL[(len(tL) // 2) - 1] + tL[len(tL) // 2]), sum(tL) / len(tL)))
+print('List: %s - Sum: %i - Median: %.1f - Avg: %.2f' % (', '.join(map(str, tL)), sum(tL), tL[len(tL)//2] if len(tL) % 2 == 1 else (tL[(len(tL) // 2) - 1] + tL[len(tL) // 2])/2, sum(tL) / len(tL)))
