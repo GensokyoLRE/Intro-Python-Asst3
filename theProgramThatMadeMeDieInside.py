7602d7c64929eaae888f30ff0dbf62604fdf0c08
GensokyoLRE <31522695+GensokyoLRE@users.noreply.github.com>
2018-09-25 10:44:23 -0700

    I'm done.
    Sorry to bother.

--- theProgramThatMadeMeDieInside.py
+++ theProgramThatMadeMeDieInside.py
@@ -1,12 +1,13 @@
 """
 Program: theProgramThatMadeMeDieInside.py
 Author: Phillip "Hifumi" Cuesta
-Last Mod: 9/23/18
+Last Mod: 9/25/18
 Purpose: Assignment for week 5. But... whyyyyyyyyyyyy doe.
+Side note: Won't be submitting more than one time again. Sorry to exasperate.
 """
 import random
 
 tL, theRand = [2], random.randint(5, 8)
 while len(tL) != theRand:
-    tL.append(random.randint(tL[-1], (tL[-1] * 2)))
-print('List: %s - Sum: %i - Median: %.0f - Avg: %.2f' % (', '.join(map(str, tL)), sum(tL), tL[len(tL)//2] if len(tL) % 2 == 1 else (tL[(len(tL) // 2) - 1] + tL[len(tL) // 2]), sum(tL) / 2))
+    tL.append(random.randint(tL[-1], (tL[-1] * 2)-1))
+print('List: %s - Sum: %i - Median: %.0f - Avg: %.2f' % (', '.join(map(str, tL)), sum(tL), tL[len(tL)//2] if len(tL) % 2 == 1 else (tL[(len(tL) // 2) - 1] + tL[len(tL) // 2]), sum(tL) / len(tL)))
